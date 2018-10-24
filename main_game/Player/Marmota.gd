d91013b2a934632c1e214c7bed070dca113961e0
vandalk <danielvandalk@gmail.com>
2018-10-24 17:15:07 -0300

    Reescrevi como a aceleração/desaceleração funciona pra corrigir o bug de pegar o café e bater no obstaculo, removi algumas variáveis que não estavam sendo usadas

--- main_game/Player/Marmota.gd
+++ main_game/Player/Marmota.gd
@@ -3,31 +3,28 @@ extends KinematicBody2D
 export var movement_sensitivity = 100 
 export var min_horizontal_speed = 100
 export var deceleration = 2
-export var max_falloff_speed = 10
-export var starting_lives = 3
+export var max_vertical_speed = 10
 
-var lives
 var player_position
 var coffee_timer
 var power_up_increment
 
 var is_tracking_on = false
-var is_accelerating = false
 var falloff_speed = 0
 var falloff_multiplyer = 1
+var acceleration = abs(deceleration)
+var acceleration_multiplyer = 0
 
 #### Engine Methods ####
 
 func _ready():
 	set_physics_process(true)
-	lives = starting_lives-1
 	coffee_timer = $CoffeeTimer
 
 func _physics_process(delta):
-	if lives >= 0:
-		var collision = self.move_and_collide(process_movement()*delta)
-		if collision:
-			process_collision(collision)
+	var collision = self.move_and_collide(process_movement()*delta)
+	if collision:
+		process_collision(collision)
 
 #### Class Methods ####
 
@@ -45,7 +42,7 @@ func process_movement():
 	
 	linear_velocity.y = update_falloff_speed()
 	
-	#print("Linear Velocity: %s"%[linear_velocity])
+	print("Linear Velocity: %s"%[linear_velocity])
 	#print("FallOff Multiplyer: %s"%[falloff_multiplyer])
 	
 	return linear_velocity
@@ -67,27 +64,21 @@ func track_mouse():
 	return horizontal_delta
 
 func update_falloff_speed():
-	if is_accelerating:
-		falloff_speed += deceleration * falloff_multiplyer
-		falloff_speed = clamp(falloff_speed, falloff_speed, max_falloff_speed * falloff_multiplyer)
-	else:
-		falloff_speed -= deceleration * falloff_multiplyer
-		falloff_speed = clamp(falloff_speed, -max_falloff_speed * falloff_multiplyer, falloff_speed)
+	falloff_speed += (acceleration * acceleration_multiplyer) - (deceleration * falloff_multiplyer)
+	falloff_speed = clamp(falloff_speed, max_vertical_speed * -falloff_multiplyer, max_vertical_speed * acceleration_multiplyer)
 	
 	return falloff_speed
 
 
 func enable_cofee_power_up(value, duration):
-	is_accelerating = true
-	power_up_increment = value
-	set_falloff_multiplyer(power_up_increment)
+	power_up_increment = value + 1
+	set_acceleration_multiplyer(power_up_increment)
 	coffee_timer.wait_time = duration
 	coffee_timer.start()
 
 func _on_CoffeeTimer_timeout():
 	#print("Coffee Timeout")
-	is_accelerating = false
-	set_falloff_multiplyer(-power_up_increment)
+	set_acceleration_multiplyer(-power_up_increment)
 
 
 func _on_ControlStrip_button_down():
@@ -102,3 +93,9 @@ func get_falloff_multiplyer():
 
 func set_falloff_multiplyer(value):
 	falloff_multiplyer += value
+
+func get_acceleration_multiplyer():
+	return acceleration_multiplyer
+
+func set_acceleration_multiplyer(value):
+	acceleration_multiplyer += value
